270b0f2e3a338063969ac84a77f940a4e6ef09c6
Masood <MSalman5230@gmail.com>
2026-01-05 03:10:29 +0530

    better logging

--- logging_config.py
+++ logging_config.py
@@ -3,7 +3,7 @@ import os
 
 logging_format = os.getenv(
     "LOG_FORMAT",
-    "%(levelname)s - %(message)s",
+    "%(asctime)s - %(levelname)s - %(message)s",
 )
 log_level = os.getenv("LOG_LEVEL", "INFO").upper()
 logging.basicConfig(level=log_level, format=logging_format)
